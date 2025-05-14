2c6e6f96fbad173227c7ff36f6db9689404ece50
Felipe Oliveira <feliveira8@gmail.com>
2025-05-14 14:33:24 -0300

    :construction: feat: creating product photo gallery

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
 export default function Home() {
   return (
-    <div>
+    <div className="container mx-auto p-4 md:p-8 min-h-screen">
+      <div className="w-full flex flex-col md:w-2/5">
+        <div className="w-full h-full aspect-square bg-red-500 border rounded-lg">
 
+        </div>
+        <div className="w-full mt-4 grid grid-cols-4 gap-2 sm:gap-4">
+          <div className="w-full aspect-square bg-red-500 border rounded-lg">
+
+          </div>
+          <div className="w-full aspect-square bg-red-500 border rounded-lg">
+
+          </div>
+          <div className="w-full aspect-square bg-red-500 border rounded-lg">
+
+          </div>
+          <div className="w-full aspect-square bg-red-500 border rounded-lg">
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
